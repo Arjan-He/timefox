457df49b72aa55703a263fdead231618d3a4d2f0
mac <mac@ergens.nl>
2021-02-09 22:29:03 +0100

    models - nieuwe opzet

--- tijdschrijven/models.py
+++ tijdschrijven/models.py
@@ -7,31 +7,62 @@ from django.dispatch import receiver
 from django.conf import settings #https://learndjango.com/tutorials/django-best-practices-referencing-user-model
 
 
+class Project(models.Model):
+    Titel = models.CharField(max_length=88, null=True)
+    ProjectTemplateID = models.ForeignKey('ProjectTemplate', on_delete=models.RESTRICT)
+    Omschrijving = models.CharField(max_length=256, null=True)
+    ParentID = models.ForeignKey('self', on_delete=models.CASCADE, null=True, related_name='subproject')
+    AanmakerID = models.IntegerField(null=True)
+    AanmaakDatum = models.DateField(auto_now_add=True)
+    MutatieDatum = models.DateField(auto_now=True)
+    GeldigVan = models.DateField()
+    GeldigTot = models.DateField()
+    Actief = models.BooleanField()
+    Personen = models.ManyToManyField('Persoon', through='Abonnement')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.Titel
+    class Meta:
+        verbose_name_plural = "projecten"
+
+
 class Persoon(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default=None)
-    Projecten = models.ManyToManyField('Project', through='Abonnement')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Projecten = models.ManyToManyField(Project, through='Abonnement')
 
     class Meta:
         verbose_name_plural = "personen"
 
-    # def __str__(self):
-    #     """String for representing the Model object."""
-    #     return self.username
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.username
 
 
-@receiver(post_save, sender=User)
-def create_user_persoon(sender, instance, created, **kwargs):
-    if created:
-        Persoon.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_user_persoon(sender, instance, created, **kwargs):
+#     if created:
+#         Persoon.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
-def save_user_persoon(sender, instance, **kwargs):
-    instance.persoon.save()
+# @receiver(post_save, sender=User)
+# def save_user_persoon(sender, instance, **kwargs):
+#     instance.persoon.save()
+
+
+class Abonnement(models.Model):
+    ProjectID = models.ForeignKey(Project, on_delete=models.CASCADE)
+    PersoonID = models.ForeignKey(Persoon, on_delete=models.CASCADE) 
+    OriginalObjectID = models.IntegerField()
+    AanmaakDatum = models.DateField(auto_now_add=True)
+    Zichtbaarheid = models.BooleanField()
+
+    class Meta:
+        verbose_name_plural = "abonnementen"
 
 
 class AccountSetting(models.Model):
-    PersoonID = models.ForeignKey('Persoon', on_delete=models.CASCADE)
+    PersoonID = models.ForeignKey(User, on_delete=models.CASCADE)
 
     ACCOOUNTITEMS = (
         (1, 'Dienstverband'),
@@ -53,25 +84,6 @@ class AccountSetting(models.Model):
         return self.AccountItem
 
 
-class Project(models.Model):
-    Titel = models.CharField(max_length=88, null=True)
-    ProjectTemplateID = models.ForeignKey('ProjectTemplate', on_delete=models.RESTRICT)
-    Omschrijving = models.CharField(max_length=256, null=True)
-    ParentID = models.ForeignKey('self', on_delete=models.CASCADE, null=True, related_name='subproject')
-    AanmakerID = models.IntegerField(null=True)
-    AanmaakDatum = models.DateField(auto_now_add=True)
-    MutatieDatum = models.DateField(auto_now=True)
-    GeldigVan = models.DateField()
-    GeldigTot = models.DateField()
-    Actief = models.BooleanField()
-    Personen = models.ManyToManyField('Persoon', through='Abonnement')
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.Titel
-    class Meta:
-        verbose_name_plural = "projecten"
-
 class ProjectTemplate(models.Model):
     Titel = models.CharField(max_length=128)
     Omschrijving = models.CharField(max_length=256)
@@ -79,17 +91,6 @@ class ProjectTemplate(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.Titel
-
-
-class Abonnement(models.Model):
-    ProjectID = models.ForeignKey('Project', on_delete=models.CASCADE)
-    PersoonID = models.ForeignKey('Persoon', on_delete=models.CASCADE) 
-    OriginalObjectID = models.IntegerField()
-    AanmaakDatum = models.DateField(auto_now_add=True)
-    Zichtbaarheid = models.BooleanField()
-
-    class Meta:
-        verbose_name_plural = "abonnementen"
 
 
 class GeschrevenTijd(models.Model):
