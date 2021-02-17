5ec90e038fe0ffb776ddd221c094640f1a794be9
Arjan Hegge <ahegge@home.nl>
2021-02-17 18:05:47 +0100

    projecten CRUD ingericht

--- tijdschrijven/views.py
+++ tijdschrijven/views.py
@@ -5,15 +5,22 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 
+def index(request):
+    """View function for home page of site."""
+
+    return render(request, 'index.html')
+
 class ProjectCreate(CreateView):
     model = Project
     fields=['Titel', 'ProjectTemplateID', 'Omschrijving', 'ParentID', 'AanmakerID', 'GeldigVan', 'GeldigTot', 'Actief']
     #initial = {'date_of_death': '11/06/2020'}
+    success_url = reverse_lazy('index')
 
 class ProjectUpdate(UpdateView):
     model = Project
     fields=['Titel', 'ProjectTemplateID', 'Omschrijving', 'ParentID', 'AanmakerID', 'GeldigVan', 'GeldigTot', 'Actief']
+    success_url = reverse_lazy('index')
 
 class ProjectDelete(DeleteView):
     model = Project
-    success_url = reverse_lazy('projecten')
+    success_url = reverse_lazy('index')
