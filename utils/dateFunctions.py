86044827ca210b6a5bba187178007aa167cb122b
mac <mac@ergens.nl>
2021-02-26 16:11:37 +0100

    datumfuncties lastdayofweek

--- utils/dateFunctions.py
+++ utils/dateFunctions.py
@@ -1,12 +1,21 @@
 import datetime
 
-
+# first day of week from given date
 def fdow (datum):
     return datum  - datetime.timedelta(days=datum.weekday() % 7)
 
 
+# last day of week from given date
+def ldow (datum):
+    return datum  - datetime.timedelta(days=datum.weekday() % 7) + datetime.timedelta(days=6)
+
+
+# piece represents how much letters from the day (0=all => 2=ma,di etc))
 def dagenInWeek(piece=0):
-    dagen = ['maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag']
+
+    # all days in week in dutch, 
+    dagen = ['maandag','dinsdag','woensdag','donderdag'
+            ,'vrijdag','zaterdag','zondag']
 
     if piece > 0:
         theReturn = [dag[:piece].lower() for dag in dagen]
