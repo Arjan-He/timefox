01eaf4247a909d48aae6f5f01f58df19bc931e04
mac <mac@ergens.nl>
2021-02-28 13:02:30 +0100

    dateFunctions.getDateRangeFromWeek: -1 wk weggehaald

--- utils/dateFunctions.py
+++ utils/dateFunctions.py
@@ -11,9 +11,9 @@ def ldow (datum):
     return datum  - datetime.timedelta(days=datum.weekday() % 7) + datetime.timedelta(days=6)
 
 
+# http://mvsourcecode.com/python-how-to-get-date-range-from-week-number-mvsourcecode/
 def getDateRangeFromWeek(p_year,p_week):
-
-    firstdayofweek = datetime.datetime.strptime(f'{p_year}-W{int(p_week )- 1}-1', "%Y-W%W-%w").date()
+    firstdayofweek = datetime.datetime.strptime(f'{p_year}-W{int(p_week )}-1', "%Y-W%W-%w").date()
     lastdayofweek = firstdayofweek + datetime.timedelta(days=6.9)
     return firstdayofweek, lastdayofweek
 
