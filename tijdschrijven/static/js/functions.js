4b21248c7fcd31ac2ad9661244d003a3ca317514
mac <mac@ergens.nl>
2021-03-13 10:57:45 +0100

    key press voor tijdgrid

--- tijdschrijven/static/js/functions.js
+++ tijdschrijven/static/js/functions.js
@@ -26,6 +26,24 @@ $(function() {
    
     });
 
+    // $(".ts-urenvak").on("keypress keyup blur",function (e) {
+    //     $(this).val($(this).val().replace(/[^0-9\.]/g,''));
+    //        if ((e.which != 46 || $(this).val().indexOf('.') != -1) && (e.which < 48 || e.which > 57)) {
+    //            e.preventDefault();
+    //        }
+    //  });
+
+    $('.ts-urenvak').keypress(function(event) {
+        //Allow only backspace and delete
+        if (event.keyCode != 46 && event.keyCode != 8) {
+            if (!parseInt(String.fromCharCode(event.which))) {
+                event.preventDefault();
+            }
+        }
+    });
+
+
+
 });
 
 
