470ebf66e2ee87bb097f577c00080b27d5b41bf7
mac <mac@ergens.nl>
2021-06-11 15:50:04 +0200

    forms leeg gemaakt

--- tijdschrijven/forms.py
+++ tijdschrijven/forms.py
@@ -1,7 +1,1 @@
 from django import forms
-
-
-class tijdschrijfUnit(forms.Form):
-    abonnement = forms.IntegerField(widget=forms.HiddenInput)
-    datum = forms.DateField(widget=forms.HiddenInput)
-    uren = forms.DecimalField(decimal_places=2, max_digits=4, max_value=24)
