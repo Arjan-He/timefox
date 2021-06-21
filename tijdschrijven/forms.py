6b3dfffab0636b5ddeac69e175552e45f71c3b76
mac <mac@ergens.nl>
2021-06-21 21:13:01 +0200

    form weggehaald

--- tijdschrijven/forms.py
+++ tijdschrijven/forms.py
@@ -1,7 +1,3 @@
 from django import forms
 
 
-class frmGridCell(forms.Form):
-    project_activiteit = forms.NumberInput()
-    datum = forms.DateTimeField()
-    tijdsduur = forms.DecimalField(min_value=0, max_value=24, max_digits=4, decimal_places=2)
